226f2996c6a49ea8ef02e8949cc2f2b6625a7739
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-07-04 11:27:46 -0700

    create reservation for car model and migrations

---
+++ database/migrations/2024_07_04_182541_create_car_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('car_reservations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('car_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('status', ['pending', 'confirmed', 'cancelled'])->default('pending');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('car_id')->references('id')->on('cars')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('car_reservations');
+    }
+};
